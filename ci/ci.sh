e616162b9ffc1734905ee278ffa8e79a3fffe486
Mark Ison <mark@codecutter.uk>
2019-05-29 08:36:50 +0100

    Update build to 7.0.4 and build for ARMv7 ARM64 will build, but doesn't currently pass tests due to MariaDB issues

--- ci/ci.sh
+++ ci/ci.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-version=6.3.13
+version=7.0.4
+image_name=codecutteruk/seafile
 set -e -x
 ./ci/install_deps.sh
 
 (
     cd image
-    # pip install docker-squash
-    # make base squash-base server
-    make base
-    make pro-base
-    make pro-server
+    make base server
+    # make pro-base
+    # make pro-server
 )
 
 mkdir -p /opt/seafile-data
-docker run -d --name seafile -e SEAFILE_SERVER_HOSTNAME=127.0.0.1 -v /opt/seafile-data:/shared -p 80:80 -p 443:443 seafileltd/seafile-pro:$version
-
+docker run -d --name seafile -e SEAFILE_SERVER_HOSTNAME=127.0.0.1 -v /opt/seafile-data:/shared -p 80:80 -p 443:443 $image_name:$version-$TAG_ARCH
 
 cat > doc.md <<EOF
 # Doc
@@ -30,20 +28,20 @@ docker restart seafile
 sleep 30
 python ci/validate_file.py doc.md
 docker rm -f seafile
-docker run -d --name seafile -e SEAFILE_SERVER_HOSTNAME=127.0.0.1 -v /opt/seafile-data:/shared -p 80:80 -p 443:443 seafileltd/seafile-pro:$version
+docker run -d --name seafile -e SEAFILE_SERVER_HOSTNAME=127.0.0.1 -v /opt/seafile-data:/shared -p 80:80 -p 443:443 $image_name:$version-$TAG_ARCH
 sleep 30
 python ci/validate_file.py doc.md
 
 rm -rf doc.md
 
-if [[ $TRAVIS_TAG =~ ^v([0-9]*?)(\.([0-9])*?){2}-pro$ ]]; then
-    ci/publish-pro-image.sh
-elif [[ $TRAVIS_TAG =~ ^v([0-9]*?)(\.([0-9])*?){2}$ ]]; then
-    ci/publish-image.sh
-elif [[ $TRAVIS_TAG =~ ^seafile-pro-base$ ]]; then
-    ci/publish-pro-base.sh
-elif [[ $TRAVIS_TAG =~ ^seafile-base$ ]]; then
-    ci/publish-base.sh
-else
-    echo "Not going to push the image to docker hub, since it's not a build triggered by a tag"
-fi
+# if [[ $TRAVIS_TAG =~ ^v([0-9]*?)(\.([0-9])*?){2}-pro$ ]]; then
+#     ci/publish-pro-image.sh
+# elif [[ $TRAVIS_TAG =~ ^v([0-9]*?)(\.([0-9])*?){2}$ ]]; then
+#     ci/publish-image.sh
+# elif [[ $TRAVIS_TAG =~ ^seafile-pro-base$ ]]; then
+#     ci/publish-pro-base.sh
+# elif [[ $TRAVIS_TAG =~ ^seafile-base$ ]]; then
+#     ci/publish-base.sh
+# else
+#     echo "Not going to push the image to docker hub, since it's not a build triggered by a tag"
+# fi
